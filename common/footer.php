5dcb4420b424fd8e61de9895643d1cefc8c57627
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2023-02-14 21:46:19 +0530

    Add files via upload

---
+++ common/footer.php
@@ -0,0 +1,50 @@
+<!-- FOOTER SECTION STARTS -->
+					
+				<div class="footer col-sm-12">
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+						National Tourism Management Authority <br> Bangalore, India
+						</div>
+				
+						<div class="footerText">
+							E-mail: queries@tourism_management.com
+						</div>
+						
+					</div>
+					
+					
+					<div class="col-sm-4">
+					</div>
+				
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Follow us on
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								
+							</div>
+						
+							<div class="tw">
+								
+							</div>
+						
+						
+						</div> <!-- social links -->
+						
+					</div>
+						
+					
+							
+				</div> <!-- footer -->
+				
+			<!-- FOOTER SECTION ENDS -->
